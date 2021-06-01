d4a5cd3d2a10e6c5932d838711a46584542beae5
LoloPopoPy <legregam@insa-toulouse.fr>
2021-06-01 13:35:03 +0200

    Finished commenting visualizer.py

--- nmrquant/engine/visualizer.py
+++ nmrquant/engine/visualizer.py
@@ -13,7 +13,7 @@ from nmrquant.engine.utilities import list_average
 class Colors:
     """Color component class for the different plotting classes"""
 
-    IndHistA_colors = cc.glasbey_bw[:]
+    IndHistA_colors = cc.glasbey_bw[:] # Individual colors from large colormap
 
     IndLineA_colors = {"grey_scale": cc.CET_L1, "red_to_yellow": cc.CET_L3, "blue_scale": cc.CET_L6,
                        "green_scale": cc.CET_L14, "darkred_scale": cc.CET_L13}
@@ -56,8 +56,8 @@ class Colors:
         :param conditions_list: list of the different conditions
         :return: list of colors
         """
-        colorlist = []
 
+        colorlist = []
         for ind, condition in enumerate(OrderedSet(conditions_list)):
             color = cc.glasbey_bw[ind]
             x = conditions_list.count(condition)
@@ -80,6 +80,7 @@ class HistPlot(ABC):
         self.data = input_data
         self.display = display
 
+        # The spectrum column is useless for plotting
         if "# Spectrum#" in input_data.index.names:
             self.data = self.data.droplevel("# Spectrum#")
 
@@ -91,7 +92,7 @@ class HistPlot(ABC):
         if "Conditions" not in self.data.index.names:
             raise IndexError("Conditions column not found in index")
 
-        # Histograms are not meant to give cinetic representations of the data, so we check in base class that no more
+        # Histograms are not meant to give kinetic representations of the data, so we check in base class that no more
         # than one time point is present in data. If that is the case, then we can safely drop the "Time_Points" from
         # the index
         if "Time_Points" in self.data.index.names:
@@ -101,7 +102,7 @@ class HistPlot(ABC):
                 self.data.droplevel("Time_Points")
 
         self.metabolite = metabolite
-
+        # We arrange the x labels, x ticks and y values here because they are the same for all histograms
         self.x_labels = list(self.data.index)
         self.x_ticks = np.arange(1, 1 + len(self.x_labels))
         self.y = self.data[self.metabolite].values
@@ -130,10 +131,8 @@ class IndHistA(HistPlot):
     def __init__(self, input_data, metabolite, display):
 
         super().__init__(input_data, metabolite, display)
-
         self.colors = Colors.IndHistA_colors
-
-        # Same thinking as for time points in the base class
+        # Same thinking for replicates as for time points in the base class
         if "Replicates" in self.data.index.names:
             if len(self.data.index.get_level_values("Replicates").unique()) > 1:
                 raise IndexError("Data should not contain more than one replicate")
@@ -143,15 +142,12 @@ class IndHistA(HistPlot):
     def build_plot(self):
 
         fig, ax = plt.subplots()
-
         ax.bar(self.x_ticks, self.y, color=self.colors)
         ax.set_xticks(self.x_ticks)
         ax.set_xticklabels(self.x_labels, rotation=45, ha="right", rotation_mode="anchor")
         ax.set_title(self.metabolite)
         ax.set_ylabel("Concentration in mM")
-
         fig.tight_layout()
-
         return fig
 
 
@@ -167,21 +163,16 @@ class IndHistB(HistPlot):
     def __init__(self, input_data, metabolite, display):
 
         super().__init__(input_data, metabolite, display)
-
-        for i in ["Conditions", "Replicates"]:
-            if i not in self.data.index.names:
-                raise KeyError(f"{i} is missing from index")
-
+        # Condition check done in base class so here we only check for replicates
+        if "Replicates" not in self.data.index.names:
+            raise KeyError("'Replicates' is missing from index")
         # We natural sort the index here so that when bar plot is initialized the data is ordered logically
         self.data = self.data.reindex(natsorted(self.data.index))
-
         # Labels should show condition and replicate number
         self.x_labels = [str(ind1) + "_" + str(ind2) for ind1, ind2
                          in zip(self.data.index.get_level_values("Conditions"),
                                 self.data.index.get_level_values("Replicates"))]
-
         self.x_ticks = np.arange(1, 1 + len(self.x_labels))
-
         # In case for some reason conditions are not in index but in columns, we try both. If we can't access the
         # conditions then we raise an error
         try:
@@ -200,11 +191,10 @@ class MultHistB(HistPlot):
     def __init__(self, input_data, std_data, metabolite, display):
 
         super().__init__(input_data, metabolite, display)
-
         self.data = input_data
         self.stds = std_data
         self.yerr = self.stds[self.metabolite].values
-
+        # Same as IndHistB for colors
         try:
             self.colors = Colors.IndHistB_colorgen(list(self.data.index.get_level_values("Conditions")))
         except KeyError:
@@ -215,14 +205,11 @@ class MultHistB(HistPlot):
     def build_plot(self):
 
         fig, ax = plt.subplots()
-
         ax.bar(self.x_ticks, self.y, color=self.colors, yerr=self.yerr)
         ax.set_xticks(self.x_ticks)
         ax.set_xticklabels(self.x_labels, rotation=45, ha="right", rotation_mode="anchor")
         ax.set_title(self.metabolite)
-
         fig.tight_layout()
-
         return fig
 
 
@@ -242,23 +229,22 @@ class LinePlot(ABC):
         self.data = self.data.loc[:, metabolite]
         self.display = display
         self.y_min = 0
-
-        # Conditions are always needed to generate the plots, even if there is only one. Line plots show cinetic data,
+        # Initialize maxes list that will be useful for calculating top y_limit in plots
+        self.maxes = []
+        # Conditions are always needed to generate the plots, even if there is only one. Line plots show kinetic data,
         # so in this case the "Time_Points" index is necessary. We check for this here
         for i in ["Conditions", "Time_Points"]:
             if i not in self.data.index.names:
                 raise IndexError(f"{i} not found in index")
-
+        # As for histograms, Spectrum column useless for plotting
         if "# Spectrum#" in input_data.index.names:
             self.data = self.data.droplevel("# Spectrum#")
-
         if "# Spectrum#" in input_data.columns:
             self.data = self.data.drop("# Spectrum#", axis=1)
 
     def __call__(self):
 
         fig = self.build_plot()
-
         return fig
 
     @staticmethod
@@ -282,7 +268,6 @@ class NoRepIndLine(LinePlot):
     def __init__(self, input_data, metabolite, display):
 
         super().__init__(input_data, metabolite, display)
-
         if "Replicates" in self.data.index.names:
             if len(self.data.index.get_level_values("Replicates").unique()) > 1:
                 raise IndexError("Too many replicates for this type of plot")
@@ -292,21 +277,21 @@ class NoRepIndLine(LinePlot):
     def build_plot(self):
 
         fig, ax = plt.subplots()
-        maxes =[]
-
+        # We generate lines in the plot for each condition
         for condition in self.data.index.get_level_values("Conditions").unique():
             tmp_df = self.data[condition]
+            # We get time points at each pass in case one condition has more or less of them
             x = list(tmp_df.index.get_level_values("Time_Points"))
             y = list(tmp_df.values)
-            maxes.append(max(y))
+            self.maxes.append(max(y))
             ax.plot(x, y, label=condition)
-        if len(maxes) == 1:
-            ax.set_ylim(bottom=self.y_min, top=maxes + (maxes / 5))
+        # We make sure we have the right value for top y limit
+        if len(self.maxes) == 1:
+            ax.set_ylim(bottom=self.y_min, top=self.maxes + (self.maxes / 5))
         else:
-            ax.set_ylim(bottom=self.y_min, top=max(maxes) + (max(maxes) / 5))
+            ax.set_ylim(bottom=self.y_min, top=max(self.maxes) + (max(self.maxes) / 5))
         fig.legend()
         ax.set_title(f"{self.metabolite}")
-
 
 
 class IndLine(LinePlot):
@@ -314,18 +299,17 @@ class IndLine(LinePlot):
     def __init__(self, input_data, metabolite, display):
 
         super().__init__(input_data, metabolite, display)
-
         if "Replicates" not in self.data.index.names:
             raise IndexError("Replicates column not found in index")
-
         self.conditions = self.data.index.get_level_values("Conditions").unique()
         self.data = self.data.reorder_levels([0, 2, 1])
         self.dicts = {}
-
+        # For indipendent line plots, for each condition the replicates must be plotted. We create a dict of dicts
+        # containing the data. This helps us take into account any inconsistencies in the data (like a missing replicate
+        # for example)
         for condition in self.conditions:
             df = self.data.loc[condition]
             repdict = {}
-
             for rep in df.index.get_level_values("Replicates"):
                 df2 = df.loc[rep, :]
                 repdict.update({rep: {"Times": list(df2.index.get_level_values("Time_Points")),
@@ -339,27 +323,29 @@ class IndLine(LinePlot):
 
     def build_plot(self):
 
-        figures, maxes = [], []
-
+        figures = []
+        # We get the maximum number of replicates possible to generate the color maps for each condition
         max_number_reps = max([max(self.dicts[i].keys()) for i in self.dicts.keys()])
         color_lists = Colors.IndLineA_colorgen(len(self.conditions), max_number_reps)
 
         for condition, c_list in zip(self.conditions, color_lists):
             fig, ax = plt.subplots()
-
+            # We build the line plots line by line aka replicate by replicate
             for rep, color in zip(self.dicts[condition].keys(), c_list):
                 x = self.dicts[condition][rep]["Times"]
                 y = self.dicts[condition][rep]["Values"]
-                maxes.append(max(y))
+                self.maxes.append(max(y)) # For y limit
                 ax.plot(x, y, color=color, label=f"Replicate {rep}")
 
-            ax.set_ylim(bottom=self.y_min, top=max(maxes) + (max(maxes) / 5))
+            y_lim = max(self.maxes) + (max(self.maxes) / 5)
+            self.maxes = [] # Reset maxes or max of each condition will be kept at each iteration
+            ax.set_ylim(bottom=self.y_min, top=y_lim)
             ax.set_title(f"{self.metabolite}\n{condition}")
             ax.set_ylabel("Concentration in mM")
             ax.set_xlabel("Time in hours")
             ax.legend()
 
-            fname = f"{self.metabolite}_{condition}"
+            fname = f"{self.metabolite}_{condition}" # For saving the plot
 
             if self.display:
                 fig.show()
@@ -370,20 +356,25 @@ class IndLine(LinePlot):
 
 
 class MeanLine(IndLine):
+    """Line plots with meaned replicates for each time point. We inherit from IndLine to initialize the dict containing
+    all the data for all the replicates. We will then calculate means and SDs from this data."""
 
     def __init__(self, input_data, metabolite, display):
 
         super().__init__(input_data, metabolite, display)
-
-        self.mean_dict = {}
-        self.std_dict = {}
-
+        self.mean_dict = {} # For calculating means
+        self.std_dict = {} # For SDs (and error bars)
+        # Sort the time points to get them in the right order
         self.times = sorted(list(self.data.index.get_level_values("Time_Points").unique()))
-
+        # Now the fun begins. We start by opening a loop through every condition/dict.key
         for condition in self.dicts.keys():
+            # We create a temporary dict that will contain each replicate's value for each time
             tmp_dict = {}
             for time in self.times:
                 time_values = []
+                # For each time, we need to get the associated value of each replicate, so we get the indice at which
+                # the time point is found in "Times" and index in "Values" to get the associated rep value. If the time
+                # is not present in "Times", a value error is caught and we continue to the next.
                 for rep in self.dicts[condition].keys():
                     try:
                         ind = self.dicts[condition][rep]["Times"].index(time)
@@ -391,9 +382,10 @@ class MeanLine(IndLine):
                         continue
                     else:
                         time_values.append(self.dicts[condition][rep]["Values"][ind])
+                # Finish up by updating the temporary dict with the values, and then the mean dictionary.
                 tmp_dict.update({time: time_values})
             self.mean_dict.update({condition: tmp_dict})
-
+        # All that is left now is calculate the means and the associated
         for condition in self.mean_dict.keys():
             tmp_dict = {}
             for time in self.mean_dict[condition].keys():
@@ -403,21 +395,21 @@ class MeanLine(IndLine):
 
     def build_plot(self):
 
-        maxes = []
         fig, ax = plt.subplots()
-        plt.subplots_adjust(right=0.8)
+        plt.subplots_adjust(right=0.8) # We make space for the legend
 
         colors = Colors.IndHistB_colorgen(list(self.mean_dict.keys()))
 
+        # We build the plot line by line aka per condition
         for condition, c in zip(self.mean_dict.keys(), colors):
             x = list(self.mean_dict[condition].keys())
             y = list(self.mean_dict[condition].values())
-            maxes.append(max(y))
+            self.maxes.append(max(y))
             yerr = list(self.std_dict[condition].values())
             ax.plot(x, y, label=condition, color=c)
             ax.errorbar(x, y, yerr=yerr, capsize=5, fmt="none", color=c)
 
-        ax.set_ylim(bottom=self.y_min, top=max(maxes) + (max(maxes) / 5))
+        ax.set_ylim(bottom=self.y_min, top=max(self.maxes) + (max(self.maxes) / 5))
         ax.set_title(f"{self.metabolite}")
         ax.set_ylabel("Concentration in mM")
         ax.set_xlabel("Time in hours")
