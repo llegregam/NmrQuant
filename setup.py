e3786be025077a359e0357a6859d797428725d54
LoloPopoPy <legregam@insa-toulouse.fr>
2021-07-22 15:42:53 +0200

    Added Ordered_set to setup.py requirements

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',  # Required
+    version='1.2.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -130,7 +130,8 @@ setup(
                       'ipywidgets',
                       'openpyxl',
                       'natsort',
-                      'xlrd'],  # Optional
+                      'xlrd',
+                      'ordered_set'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
