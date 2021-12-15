d2c278effe2a6222ca6f3594b922cf84e006af49
LoloPopoPy <legregam@insa-toulouse.fr>
2021-12-15 09:40:25 +0100

    Bump to 1.2.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Loic Le Gregam'
 author = 'Loic Le Gregam'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.7'
+release = '1.2.7.1'
 
 
 # -- General configuration ---------------------------------------------------
